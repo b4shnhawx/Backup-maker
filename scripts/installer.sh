2e4ad9c30ade45b84eb55d16af211dd6e26fbebf
David Arribas Hidalgo <45437051+davidahid@users.noreply.github.com>
2019-04-25 00:13:53 +0200

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd
-sudo cp $HOME/Backup-maker/scripts/..... /etc/
+sudo cp $HOME/Backup-maker/scripts/Backup_maker.sh /etc/
 sudo cp $HOME/Backup-maker/scripts/bckpm /bin/
 sudo chmod u+x /bin/bckpm
